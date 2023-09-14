678fc3e8bb2ab296f8ac011aa5f0399ca8f9ef3d
acsagum <144298916+acsagum@users.noreply.github.com>
2023-09-14 10:48:50 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,17 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello Everyone! My Name Is Adrian Sagum!</p>
+    <h1>10 Things That Require Zero Talent</h1>
+    <p1>1. Being On Time</p1>
+    <p1>2. Making An Effort</p1>
+    <p1>3. Being High Energy</p1>
+    <p1>4. Having A Positive Attitude</p1>
+    <p1>5. Being Passionate</p1>
+    <p1>6. Using Good Body Language</p1>
+    <p1>7. Being Coachable</p1>
+    <p1>8. Doing A Little Extra</p1>
+    <p1>9. Being Prepared</p1>
+    <p1>10. Having A Strong Work Ethic</p1>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
